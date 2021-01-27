db482f9256466c6d450beb749721f45c61946d89
Renato <renatobmpsilva@gmail.com>
2021-01-27 00:26:31 -0300

    first

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+// images
+// import Img1 from '../src/assets/img-01.jpg';
+// import Img2 from '../src/assets/img-02.jpg';
+// import Img3 from '../src/assets/img-03.jpg';
+// import Img4 from '../src/assets/img-04.jpg';
+// import Img5 from '../src/assets/img-05.jpg';
+// import Img6 from '../src/assets/img-06.jpg';
+// import Img7 from '../src/assets/img-07.jpg';
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
 }
