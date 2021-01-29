01baf415949ea7ded3741fc23b2bf04cf99f4afd
Renato <renatobmpsilva@gmail.com>
2021-01-29 00:23:03 -0300

    theme

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 
+import React, {useState} from 'react';
 import styled from 'styled-components'
 import db from '../db.json';
 import Widget from '../src/components/Widget'
@@ -7,14 +8,6 @@ import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner';
 import Head from 'next/head';
-// images
-// import Img1 from '../src/assets/img-01.jpg';
-// import Img2 from '../src/assets/img-02.jpg';
-// import Img3 from '../src/assets/img-03.jpg';
-// import Img4 from '../src/assets/img-04.jpg';
-// import Img5 from '../src/assets/img-05.jpg';
-// import Img6 from '../src/assets/img-06.jpg';
-// import Img7 from '../src/assets/img-07.jpg';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -28,8 +21,22 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const [theme, setTheme] = useState('#2a327d');
+  const [icon, setIcon] = useState('/assets/icon-rd.png');
+  const [background, setBackground] = useState(db.bg);
+  const handleTheme = () => {
+    if(theme != 'black'){
+      setTheme('black');
+      setIcon('/assets/icon-tr.png');
+      setBackground('https://images4.alphacoders.com/977/977093.jpg');
+    }else{
+      setTheme('#2a327d');
+      setIcon('/assets/icon-rd.png');
+      setBackground(db.bg);
+    }
+  }
   return (
-    <QuizBackground backgroundImage={db.bg}>
+    <QuizBackground backgroundImage={background}>
       <Head>
         <title>Spiderman | Alura Quiz</title>
         <meta name="title" content="Spiderman | Alura Quiz" />
@@ -44,8 +51,12 @@ export default function Home() {
         <meta property="twitter:title" content="Spiderman | Alura Quiz" />
         <meta property="twitter:description" content="Quiz feito com muito carinho durante a Imersão React+Nextjs da Alura. Apresentado pelo incrível time: Mario Souto, Juliana Amoasei e Paulo Silveira." />
         <meta property="twitter:image" content="/assets/icon-rd.png" />
+        <link rel="icon" href={icon} />
+        <meta name="theme-color" content={theme} />
+        <link rel="manifest" href="/manifest.json"></link>
       </Head>
       <QuizContainer>
+        <button onClick={handleTheme}>Theme</button>
         <QuizLogo />
         <Widget>
           <Widget.Header>
